244b59d7038b92e8895db0a9e4c2ac63fb6d0ca1
zbrle <zbrlekovic@gmail.com>
2023-09-17 10:35:36 +0200

    Fix kotlin dependency issue

--- app/src/main/java/com/example/beer/MainActivity.kt
+++ app/src/main/java/com/example/beer/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.beer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.beer.ui.theme.BeerTheme
