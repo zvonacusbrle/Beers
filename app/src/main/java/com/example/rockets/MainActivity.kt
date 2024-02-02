b3d69291b0e28ce2f9c68b1eb90ee9b6daadb90c
zbrle <zbrlekovic@gmail.com>
2024-02-02 10:57:17 +0100

    Add api in data module and repository in domain module

--- app/src/main/java/com/example/rockets/MainActivity.kt
+++ app/src/main/java/com/example/rockets/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.rockets
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.adaptive.calculateDisplayFeatures
 import com.google.firebase.FirebaseApp
 import com.google.firebase.crashlytics.FirebaseCrashlytics
@@ -19,7 +15,9 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainViewModel by viewModels()
+   // private val viewModel: MainViewModel by viewModels()
+
+    // private val viewModel: MainViewModel by viewModels()
 
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,32 +34,4 @@ class MainActivity : ComponentActivity() {
             // val uiState by viewModel.uiState.collectAsStateWithLifecycle()
         }
     }
-
-    private fun setupObserver() {
-        lifecycleScope.launch {
-            viewModel.getRockets().observe(this@MainActivity) {
-                when (it) {
-                    is Resource.Loading -> {
-                        Log.d(TAG, "Loading - Called")
-                    }
-                    is Resource.Success -> {
-                        Log.d(TAG, "Success - Called ${it.data}")
-                        // Toast.makeText(this@MainActivity, "${it.data}", Toast.LENGTH_SHORT).show()
-                    }
-                    is Resource.Error -> {
-                        Log.d(TAG, "Error - Called $it")
-                        // Handle error case
-                    }
-
-                    else -> {}
-                }
-            }
-        }
-    }
-}
-
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Loading<T>(data: T? = null) : Resource<T>(data)
-    class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(data: T? = null, message: String) : Resource<T>(data, message)
 }
