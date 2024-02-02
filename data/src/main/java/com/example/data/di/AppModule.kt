191f9c4d2929d5b7883b241be32c38c8707330eb
zbrle <zbrlekovic@gmail.com>
2024-02-02 10:57:17 +0100

    Add api in data module and repository in domain module

--- data/src/main/java/com/example/data/di/AppModule.kt
+++ data/src/main/java/com/example/data/di/AppModule.kt
@@ -1,30 +1,14 @@
 package com.example.data.di
 
 import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.network.okHttpClient
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import javax.inject.Singleton
+import org.koin.dsl.module
 
-@Module
-@InstallIn(SingletonComponent::class)
-object ApoModule {
-
-    @Singleton
-    @Provides
-    fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder().build()
-    }
-
-    @Singleton
-    @Provides
-    fun provideApolloClient(okHttpClient: OkHttpClient): ApolloClient {
-        return ApolloClient.Builder()
-            .serverUrl("https://spacex-production.up.railway.app/")
-            .okHttpClient(okHttpClient)
-            .build()
+val apiModule = {
+    module {
+        single {
+            ApolloClient.Builder()
+                .serverUrl("https://spacex-production.up.railway.app/")
+                .build()
+        }
     }
 }
