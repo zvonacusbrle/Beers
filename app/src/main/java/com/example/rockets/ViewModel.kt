7ebeb1d4c143a23f6fd8cac92d9ab3e2cfc1066a
zbrle <zbrlekovic@gmail.com>
2024-02-28 21:02:52 +0100

    Add fetch rockets use case

---
+++ app/src/main/java/com/example/rockets/ViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.rockets
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.repository.RocketsRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class ViewModel(
+    private val repository: RocketsRepository,
+
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(HomeViewState())
+    private val refreshing = MutableStateFlow(false)
+
+
+    val state: StateFlow<HomeViewState>
+        get() = _state
+
+    init {
+        viewModelScope.launch {
+
+        }
+    }
+
+
+}
+
+
+
+data class HomeViewState(
+
+    val refreshing: Boolean = false,
+    val errorMessage: String? = null
+
+)
--- data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.example.data.di
 
 import com.apollographql.apollo3.ApolloClient
+import com.example.data.network.MyApi
 import com.example.domain.repository.RocketsRepository
 import com.example.repository.RocketsRepositoryImpl
 import org.koin.dsl.module
