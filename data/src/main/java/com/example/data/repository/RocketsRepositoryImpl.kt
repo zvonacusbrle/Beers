0fee2fe5f4bc0f67102e53f2c40624d70ce00c12
zbrle <zbrlekovic@gmail.com>
2024-03-25 18:26:39 +0100

    Modify RocketsRepo to get List of Rockets in a flow

--- data/src/main/java/com/example/data/repository/RocketsRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/RocketsRepositoryImpl.kt
@@ -1,16 +1,30 @@
 package com.example.data.repository
 
-import com.apollographql.apollo3.ApolloCall
-
+import com.apollographql.apollo3.ApolloClient
 import com.data.RocketsQuery
-import com.example.data.network.MyApi
+import com.example.domain.model.Rocket
 import com.example.domain.repository.RocketsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 
 class RocketsRepositoryImpl(
-    private val myApi: MyApi
+    private val apolloClient: ApolloClient
 ) : RocketsRepository {
-    override suspend fun rocketsQuery(): ApolloCall<RocketsQuery.Rocket> {
-        return myApi.fetchRockets()
+    override suspend fun fetchRockets(): Flow<List<Rocket>?> = flow{
+        val response = apolloClient.query(RocketsQuery()).execute()
+        val rockets = response.data?.rockets?.mapNotNull {
+            it?.asRocket()
+        }
+
+        emit(rockets)
     }
+}
+
+fun RocketsQuery.Rocket.asRocket(): Rocket {
+    return Rocket(
+        id = this.id ?: "",
+        name = this.name ?: "",
+        description = this.description ?: ""
+    )
 }
--- domain/src/main/java/com/example/domain/repository/RocketsRepository.kt
+++ domain/src/main/java/com/example/domain/repository/RocketsRepository.kt
@@ -3,8 +3,9 @@ package com.example.domain.repository
 import com.apollographql.apollo3.ApolloCall
 import com.apollographql.apollo3.api.Operation
 import com.example.domain.model.Rocket
+import kotlinx.coroutines.flow.Flow
 
 
 interface RocketsRepository{
-    suspend fun rocketsQuery() : ApolloCall<Rocket>
+    suspend fun fetchRockets() : Flow<List<Rocket>?>
 }
