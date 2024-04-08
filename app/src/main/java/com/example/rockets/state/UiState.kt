d6e78dff1e935d741d25710448bef419c51eb621
zbrle <zbrlekovic@gmail.com>
2024-04-08 18:34:43 +0200

    Add CommonResultConverter for UiState

--- app/src/main/java/com/example/rockets/state/UiState.kt
+++ app/src/main/java/com/example/rockets/state/UiState.kt
@@ -3,4 +3,6 @@ package com.example.rockets.state
 sealed class UiState<out T : Any> {
     data class Error(val errorMessage: String) : UiState<Nothing>()
     data class Success<T : Any>(val data: T) : UiState<T>()
+
+    object Loading : UiState<Nothing>()
 }
