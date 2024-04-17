345628950d5014568dffe96736ffa7d4bb611122
zbrle <zbrlekovic@gmail.com>
2024-04-17 21:24:40 +0200

    Modify List<Rocket> to RocketsList

--- data/src/main/java/com/example/data/repository/RocketsRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/RocketsRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.example.data.repository
 import com.apollographql.apollo3.ApolloClient
 import com.data.RocketsQuery
 import com.example.domain.Entity.Rocket
+import com.example.domain.Entity.RocketsList
 import com.example.domain.repository.RocketsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -11,19 +12,16 @@ import kotlinx.coroutines.flow.flow
 class RocketsRepositoryImpl(
     private val apolloClient: ApolloClient
 ) : RocketsRepository {
-    override suspend fun getRockets(): Flow<List<Rocket>> = flow{
+    override suspend fun getRockets(): Flow<RocketsList> = flow {
         val response = apolloClient.query(RocketsQuery()).execute()
-        val rockets = response.data?.rockets?.mapNotNull {rocket ->
-            rocket?.asRocket()
-        } ?: emptyList()
-
-        emit(rockets)
+        val rockets = response.data?.rockets?.mapNotNull { rocket ->
+            rocket?.asRockets()
+        }
+        emit(RocketsList(rockets ?: emptyList()))
     }
-
-
 }
 
-fun RocketsQuery.Rocket.asRocket(): Rocket {
+fun RocketsQuery.Rocket.asRockets(): Rocket {
     return Rocket(
         id = this.id ?: "",
         name = this.name ?: "",
